cf4e784d64224dc40147a6e14dcec35dffeebfb1
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-14 09:52:53 +0530

    Create help.py

---
+++ handlers/help.py
@@ -0,0 +1,17 @@
+from telegram.ext import CommandHandler
+
+
+def help(update, context):
+    update.effective_message.reply_text(
+        "Hey there,.\nI am an AI ChatBot, Developed with love for interacting with users! \nI am developed for having chats but not on private \nFor Activation of my AI you must add me on group and type /start@AlexaChatRoBot & Done \nFrom then i can talk with you all in groups!!"
+    )
+
+
+__handlers__ = [
+    [
+        CommandHandler(
+            "help",
+            help
+        )
+    ]
+]
