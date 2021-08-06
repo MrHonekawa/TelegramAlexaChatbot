98c6c7a3ae2c8b10e730a8fc0cece20b39d19037
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-08-06 13:25:47 +0530

    improvement

--- handlers/help.py
+++ handlers/help.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler
 
 def help(update, context):
     update.effective_message.reply_text(
-        "Hey there,.\nI am an AI ChatBot, Developed with love for interacting with users! \nI am developed for having chats but not on private \nFor Activation of my AI you must add me on group and type /start@AlexaChatRoBot & Done \nFrom then i can talk with you all in groups!!"
+        "Hey there,.\nI am an AI ChatBot, Developed with love for interacting with users! \nI am developed for having chats but not on private \nFor Activation of my AI you must add me on group and type /alexa & Done \nFrom then i can talk with you all in groups!!"
     )
 
 
